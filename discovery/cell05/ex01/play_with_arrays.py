9190e62d23caad2b43317c686241193b742e9e6b
paoza0011 <67050295@kmitl.ac.th>
2026-09-23 15:13:39 +0700

    Add cell05 ex01

---
+++ discovery/cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original_array:
+    new_array.append(number + 2)
+
+print("Original array:", original_array)
+print("New array:", new_array)
