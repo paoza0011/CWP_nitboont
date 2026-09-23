eefbfe6df9eaa8a5c867989e5a825cc5085909d0
paoza0011 <67050295@kmitl.ac.th>
2026-09-23 19:05:03 +0700

    Add cell07 exercise 02 help_your_professor

---
+++ discovery_piscine/cell07/ex02/help_your_professor.py
@@ -0,0 +1,20 @@
+def average(class_scores):
+    return sum(class_scores.values()) / len(class_scores)
+
+
+class_3B = {
+    "marie": 18,
+    "jean": 15,
+    "coline": 8,
+    "luc": 9
+}
+
+class_3C = {
+    "quentin": 17,
+    "julie": 15,
+    "marc": 8,
+    "stephanie": 13
+}
+
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
