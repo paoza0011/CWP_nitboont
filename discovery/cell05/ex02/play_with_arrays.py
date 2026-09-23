95b75617f986081d7bfe656b36a94c26428b9b3f
paoza0011 <67050295@kmitl.ac.th>
2026-09-23 15:25:41 +0700

    Add cell05 ex02

---
+++ discovery/cell05/ex02/play_with_arrays.py
@@ -0,0 +1,10 @@
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original_array:
+    if number > 5:
+        new_array.append(number + 2)
+
+print(original_array)
+print(new_array)
