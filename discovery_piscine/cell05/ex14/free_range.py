1daf9ed61d84bcff4a8b0e68f2f697ae041ef849
paoza0011 <67050295@kmitl.ac.th>
2026-09-23 17:58:09 +0700

    Add exercise 14 free_range

---
+++ discovery_piscine/cell05/ex14/free_range.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    first = int(sys.argv[1])
+    second = int(sys.argv[2])
+
+    numbers = list(range(first, second + 1))
+    print(numbers)
